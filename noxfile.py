d1cd4560e9ab29b3cf8e7ddd3e3253afa2acb329
ak1ra <git@ak1ra.xyz>
2025-06-11 22:09:53 +0800

    test: disable nox tests session

--- noxfile.py
+++ noxfile.py
@@ -5,10 +5,10 @@ import nox
 os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
 
 
-@nox.session
-def tests(session: nox.Session) -> None:
-    session.run_always("pdm", "install", "-G", "tests", external=True)
-    session.run("pytest", "-v", "tests/")
+# @nox.session
+# def tests(session: nox.Session) -> None:
+#     session.run_always("pdm", "install", "-G", "tests", external=True)
+#     session.run("pytest", "-v", "tests/")
 
 
 @nox.session
