0f44263a2cf16d4e9ce86ef50f8ef7cff7b71e16
ak1ra <git@ak1ra.xyz>
2025-10-30 17:23:07 +0800

    refactor: remove logging_config and impl JsonFormatter

--- src/chaos_box/logging.py
+++ src/chaos_box/logging.py
@@ -1,91 +1,109 @@
-# https://guicommits.com/how-to-log-in-python-like-a-pro/
-# https://github.com/guilatrova/tryceratops/blob/main/src/tryceratops/logging_config.py
-# https://docs.python.org/3/library/logging.config.html#logging-config-dictschema
-
+import json
 import logging
-import logging.config
 import os
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
-
-logging_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "brief": {
-            "format": "%(message)s",
-        },
-        "precise": {
-            "format": "[%(asctime)s][%(name)s][%(lineno)d][%(levelname)s] %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-        },
-    },
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-            "formatter": "brief",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "root": {
-        "level": "INFO",
-        "handlers": [
-            "console",
-        ],
-    },
-}
 
 logging_dir = Path(f"~/.local/state/{__package__}/log").expanduser()
 
 
+class JsonFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord):
+        log_record = {
+            "timestamp": self.formatTime(record, self.datefmt),
+            "level": record.levelname,
+            "message": record.getMessage(),
+            "name": record.name,
+            "pathname": record.pathname,
+            "lineno": record.lineno,
+            "process": record.process,
+            "thread": record.thread,
+        }
+        if record.exc_info:
+            log_record["exc_info"] = self.formatException(record.exc_info)
+        if record.stack_info:
+            log_record["stack_info"] = self.formatStack(record.stack_info)
+
+        # Add any extra attributes passed to the log call
+        if hasattr(record, "extra"):
+            log_record.update(record.extra)
+
+        return json.dumps(log_record, ensure_ascii=False, default=str)
+
+
 def setup_logger(
-    name: str, logfile: bool = False, debug: bool = os.environ.get("DEBUG", False)
+    name: str,
+    level: int = logging.INFO,
+    file_logging: bool = False,
 ) -> logging.Logger:
     """
-    Sets up and configures a logger for the application.
+    Setup root logger,
+    Can only be configured once in program entrypoint
 
-    This function initializes a logger with the specified name and optional file-based logging.
-    It uses a predefined logging configuration and supports both console and file handlers.
-    If the `DEBUG` environment variable is set or `debug` is True, the logging level is set to DEBUG.
-
-    Args:
-        name (str): The name of the logger to create or retrieve.
-        logfile (bool): Whether to enable file-based logging.
-            If False, only console logging is used. Defaults to False.
-        debug (bool, optional): Whether to enable debug-level logging.
-            Defaults to the value of the `DEBUG` environment variable, or False if not set.
-
-    Returns:
-        logging.Logger: The configured logger instance.
-
-    Notes:
-        - This function should only be called once in the main entry point of the application.
-        - Other modules should use `logging.getLogger(__name__)` to retrieve their logger.
-
-    Raises:
-        OSError: If the log directory cannot be created when file-based logging is enabled.
+    https://rednafi.com/python/no-hijack-root-logger/
     """
-    if debug:
-        logging_config["root"]["level"] = "DEBUG"
+    logger = logging.getLogger()
 
-    if not logfile:
-        logging.config.dictConfig(logging_config)
+    if getattr(logger, "_setup_root_logger", False):
         return logging.getLogger(name)
 
-    if not logging_dir.exists():
-        logging_dir.mkdir(parents=True)
+    debug = bool(os.environ.get("DEBUG", False))
+    logger.setLevel(logging.DEBUG if debug else level)
 
-    logfile_handler = {
-        "logfile": {
-            "class": "logging.handlers.RotatingFileHandler",
-            "filename": logging_dir / f"{name}.log",
-            "formatter": "precise",
-            "maxBytes": 10 * 1024 * 1024,
-            "backupCount": 5,
-        }
-    }
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(console_handler)
 
-    logging_config["handlers"].update(logfile_handler)
-    logging_config["root"]["handlers"].append("logfile")
+    if file_logging:
+        logging_dir.mkdir(parents=True, exist_ok=True)
+        file_handler = RotatingFileHandler(
+            filename=logging_dir / f"{name}.log",
+            maxBytes=10 * 1024 * 1024,
+            backupCount=10,
+        )
+        file_handler.setFormatter(
+            logging.Formatter("[%(asctime)s][%(name)s][%(levelname)s][%(message)s]")
+        )
+        logger.addHandler(file_handler)
 
-    logging.config.dictConfig(logging_config)
-    return logging.getLogger(name)
+    logger._setup_root_logger = True
+
+    return logger
+
+
+def setup_json_logger(
+    name: str,
+    level: int = logging.INFO,
+    file_logging: bool = False,
+) -> logging.Logger:
+    """
+    Setup root logger in JSON format,
+    Can only be configured once in program entrypoint
+
+    https://rednafi.com/python/no-hijack-root-logger/
+    """
+    logger = logging.getLogger()
+
+    if getattr(logger, "_setup_root_logger", False):
+        return logging.getLogger(name)
+
+    debug = bool(os.environ.get("DEBUG", False))
+    logger.setLevel(logging.DEBUG if debug else level)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter("%(message)s"))
+    logger.addHandler(console_handler)
+
+    if file_logging:
+        logging_dir.mkdir(parents=True, exist_ok=True)
+        file_handler = RotatingFileHandler(
+            filename=logging_dir / f"{name}.jsonl",
+            maxBytes=10 * 1024 * 1024,
+            backupCount=10,
+        )
+        file_handler.setFormatter(JsonFormatter())
+        logger.addHandler(file_handler)
+
+    logger._setup_root_logger = True
+
+    return logger
